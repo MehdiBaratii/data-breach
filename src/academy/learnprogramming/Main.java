277dae407f5a5e5306ce856bdd7cd1eca1ac2df3
Mehdi Barati <mehdibarati22@gmail.com>
2022-09-01 12:06:06 -0400

    dd

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int result = 3;
+        System.out.println("result = "+ result);
+
+        result ++;
+        System.out.println("3 + 1 = " + result);
+
+        result *=5;
+        System.out.println("4 * 5 = " + result);
+    }
+}
